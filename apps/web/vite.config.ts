3c189b4541672f79d54c82a1e162b3b17b58df62
Michael Staton <mpstaton@gmail.com>
2025-02-19 15:33:00 -0600

    resolved errors on pnpm install, involved reconfiguring config files

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -1,1 +1,18 @@
- 
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import styleX from '@stylexjs/rollup-plugin';
+
+export default defineConfig({
+  plugins: [
+    react(),
+    styleX({
+      // StyleX options
+      dev: process.env.NODE_ENV === 'development',
+      runtimeInjection: true,
+      assumeProductionMode: process.env.NODE_ENV === 'production',
+    })
+  ],
+  server: {
+    port: 3000
+  }
+}); 
